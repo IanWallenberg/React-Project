0cecba77360464eb070dc52f4dce3c1548574bc3
Ian <ian_wallenberg@hotmail.se>
2023-06-22 16:01:33 +0200

    Prettier addition
    Prettier changed up some

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -1,39 +1,31 @@
-import React from 'react'
-import mindblown from "../Images/mindblown.gif"
-
+import React from "react";
+import mindblown from "../Images/mindblown.gif";
 
 export const ResultPage = () => {
   var request = new XMLHttpRequest();
-var item = {};
+  var item = {};
 
-request.open('GET', 'https://api.adviceslip.com/advice');
+  request.open("GET", "https://api.adviceslip.com/advice");
 
-
-request.onload = function() {
+  request.onload = function () {
     var response = request.response;
     console.log(typeof response);
     var parsedData = JSON.parse(response);
-  
-  //   GENERATING THE ADVICE WITH A LOOP
-    for(item in parsedData) {
-        var slip = parsedData[item].advice;
-        var id = document.createElement('p');
-        id.innerHTML = slip;
-        document.body.appendChild(id);
+
+    //   GENERATING THE ADVICE WITH A LOOP
+    for (item in parsedData) {
+      var slip = parsedData[item].advice;
+      var id = document.createElement("p");
+      id.innerHTML = slip;
+      document.body.appendChild(id);
     }
   };
-  
+
   request.send();
 
   return (
     <>
-    <img src={mindblown} alt="Mindblown" className="mindblown"/>
+      <img src={mindblown} alt="Mindblown" className="mindblown" />
     </>
-  )
-}
-
-
-
-
-
-  
+  );
+};
